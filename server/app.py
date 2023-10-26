28eb54d7eb981afa133cd165d6508f7ae6186219
Navita Ramasar <navitaramasar14@gmail.com>
2023-10-26 14:53:06 -0400

    updated

--- server/app.py
+++ server/app.py
@@ -14,13 +14,63 @@ migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/messages')
-def messages():
-    return ''
+@app.get('/messages')
+def get_messages():
+    mesg = Message.query.order_by(Message.created_at).all()
+    body = [m.to_dict for m in mesg]
+    return body, 200
 
-@app.route('/messages/<int:id>')
-def messages_by_id(id):
-    return ''
+@app.post('/messages')
+def post_messages():
+    msg_data = request.get_json()
+    new_msg = Message(
+        body = msg_data.get('body'),
+        username = msg_data.get('username')
+    )
+    db.session.add(new_msg)
+    db.session.commit()
+    return new_msg.to_dict(), 201
+
+@app.delete('/messages/<int:id')
+def delete_message(id):
+    msg = Message.query.filter(Message.id == id).first()
+
+    if msg is None:
+        return {'message': "message not found"}, 404
+    
+    db.session.delete(msg)
+    db.session.commit()
+
+    return {}, 200
+
+
+@app.patch('/messages/<int:id')
+def patch_message(id):
+    msg = Message.query.filter(Message.id == id).first()
+
+    if msg is None:
+        return {'message': "message not found"}, 404
+    
+    msg_data = request.get_json()
+
+    for field in msg_data:
+        setattr(msg, field, msg_data[field])
+    
+    db.session.add(msg)
+    db.session.commit()
+
+    return msg.to_dict(), 200
+
+
+@app.get('/messages/<int:id>')
+def get_messages_by_id(id):
+    msg = Message.query.filter(Message.id == id).first()
+
+    if msg is None:
+        return {'message': "message not found"}, 404
+    
+    return msg.to_dict(), 200
+
 
 if __name__ == '__main__':
     app.run(port=5555)
